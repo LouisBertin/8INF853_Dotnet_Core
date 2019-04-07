a4aa78ddc84e9ab8a3e72cbefec5c2a972f75389
LouisBertin <bertin.louis7@gmail.com>
2019-04-07 14:44:16 -0400

    securize MarquesController

--- web_mvc/Controllers/MarquesController.cs
+++ web_mvc/Controllers/MarquesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using web_mvc.Models;
 
 namespace web_mvc.Controllers
 {
+    [Authorize]
     public class MarquesController : Controller
     {
         private readonly ApplicationDbContext _context;

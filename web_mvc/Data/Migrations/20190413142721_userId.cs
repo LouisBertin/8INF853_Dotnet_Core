a475d61f5f2f437aa9aa38dbe86af5146a27a458
mikavasse <mickael.vasse.62@gmail.com>
2019-04-13 10:30:07 -0400

    reservation avec userId

---
+++ web_mvc/Data/Migrations/20190413142721_userId.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace web_mvc.Data.Migrations
+{
+    public partial class userId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Reservation",
+                newName: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Reservation",
+                newName: "UserName");
+        }
+    }
+}
